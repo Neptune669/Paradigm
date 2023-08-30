91286468a0a05dff3759684f6408767d08aa4c01
eslamheshamm <esl2mhesham@gmail.com>
2023-08-30 15:05:45 +0300

    push

--- src/blog.jsx
+++ src/blog.jsx
@@ -1,50 +1,57 @@
 const Blog = () => {
-  return (
-    <div className="max-w-7xl mx-auto mt-[8rem] p-8 ">
-      <p className="text-[#48AECD]">Blogs</p>
-      <div className="grid grid-cols-3 gap-14 mt-6">
-        <div className="relative grid h-[20rem] w-[15rem] max-w-[35rem] flex-col items-end justify-center overflow-hidden rounded-xl bg-white bg-clip-border text-center text-gray-700">
-          <div className="absolute inset-0 m-0 h-full w-full overflow-hidden rounded-none bg-transparent bg-[url('https://images.pexels.com/photos/3379261/pexels-photo-3379261.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')] bg-cover bg-clip-border bg-center text-gray-700 shadow-none">
-            <div className="to-bg-black-10 absolute inset-0 h-full w-full bg-gradient-to-t from-black/80 via-black/50"></div>
-          </div>
-          <div className="relative p-6 py-14 px-6 md:px-12">
-            <h2 className="mb-6 block font-inter text-sm font-bold leading-[1.5] tracking-normal text-white antialiased">
-              What Event Organizers Need to Know
-            </h2>
-            <h5 className="mb-4 block font-sans text-xl font-semibold leading-snug tracking-normal text-gray-400 antialiased">
-              Abril 1.2022
-            </h5>
-          </div>
-        </div>
-        <div className="relative grid h-[20rem] w-[15rem] max-w-[35rem] flex-col items-end justify-center overflow-hidden rounded-xl bg-white bg-clip-border text-center text-gray-700">
-          <div className="absolute inset-0 m-0 h-full w-full overflow-hidden rounded-none bg-transparent bg-[url('https://images.pexels.com/photos/3379261/pexels-photo-3379261.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')] bg-cover bg-clip-border bg-center text-gray-700 shadow-none">
-            <div className="to-bg-black-10 absolute inset-0 h-full w-full bg-gradient-to-t from-black/80 via-black/50"></div>
-          </div>
-          <div className="relative p-6 py-14 px-6 md:px-12">
-            <h2 className="mb-6 block font-inter font-bold text-sm  leading-[1.5] tracking-normal text-white antialiased">
-              What Event Organizers Need to Know
-            </h2>
-            <h5 className="mb-4 block font-sans text-xl font-semibold leading-snug tracking-normal text-gray-400 antialiased">
-              Abril 1.2022
-            </h5>
-          </div>
-        </div>
-        <div className="relative grid h-[20rem] w-[15rem] max-w-[35rem] flex-col items-end justify-center overflow-hidden rounded-xl bg-white bg-clip-border text-center text-gray-700">
-          <div className="absolute inset-0 m-0 h-full w-full overflow-hidden rounded-none bg-transparent bg-[url('https://images.pexels.com/photos/3379261/pexels-photo-3379261.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')] bg-cover bg-clip-border bg-center text-gray-700 shadow-none">
-            <div className="to-bg-black-10 absolute inset-0 h-full w-full bg-gradient-to-t from-black/80 via-black/50"></div>
-          </div>
-          <div className="relative p-6 py-14 px-6 md:px-12">
-            <h2 className="mb-6 block font-inter text-sm font-bold leading-[1.5] tracking-normal text-white antialiased">
-              What Event Organizers Need to Know
-            </h2>
-            <h5 className="mb-4 block font-sans text-xl font-semibold  leading-snug tracking-normal text-gray-400 antialiased">
-              Abril 1.2022
-            </h5>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div className="max-w-7xl mx-auto p-8 ">
+			<p className="text-[#48AECD] uppercase text-sm font-semibold mb-2">
+				Blog
+			</p>
+			<div className="flex justify-between items-center mb-16">
+				<h2 className="text-4xl font-medium">Our Recent Articles</h2>
+				<p className="font-semibold flex items-center gap-1">
+					<span>View all events</span>
+					<span>
+						<svg
+							width="18"
+							height="28"
+							viewBox="0 0 18 28"
+							fill="none"
+							xmlns="http://www.w3.org/2000/svg"
+						>
+							<path
+								d="M3.74988 14.1149H14.2499"
+								stroke="white"
+								strokeWidth="1.5"
+								strokeLinecap="round"
+								strokeLinejoin="round"
+							/>
+							<path
+								d="M8.99988 8.86487L14.2499 14.1149L8.99988 19.3649"
+								stroke="white"
+								strokeWidth="1.5"
+								strokeLinecap="round"
+								strokeLinejoin="round"
+							/>
+						</svg>
+					</span>
+				</p>
+			</div>{" "}
+			<div className="grid grid-cols-3 gap-14">
+				<div className="relative h-80 flex flex-col justify-end  overflow-hidden rounded-xl bg-white shadow-none ">
+					<figure className="rounded-xl">
+						<img
+							src="https://images.pexels.com/photos/3379261/pexels-photo-3379261.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+							className="absolute inset-0 h-full w-full overflow-hidden rounded-xl bg-transparent bg-cover bg-center"
+						/>
+					</figure>
+					<div className="relative pl-3 pr-12 pb-4 font-semibold">
+						<h3 className="text-white text-2xl mb-3">
+							What Event Organizers Need to Know
+						</h3>
+						<h4 className=" uppercase text-sm">APril 1.2022</h4>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Blog;
--- src/trust.jsx
+++ src/trust.jsx
@@ -1,81 +1,81 @@
 import icon from "./assets/Rectangle.png";
 import Marquee from "react-fast-marquee";
 const Trust = () => {
-  const logos = [
-    {
-      logo: <div className="h-10 w-20 bg-white rounded-lg"></div>,
-    },
-    {
-      logo: <div className="h-10 w-20 bg-white rounded-lg"></div>,
-    },
-    {
-      logo: <div className="h-10 w-20 bg-white rounded-lg"></div>,
-    },
-    {
-      logo: <div className="h-10 w-20 bg-white rounded-lg"></div>,
-    },
-    {
-      logo: <div className="h-10 w-20 bg-white rounded-lg"></div>,
-    },
-    {
-      logo: <div className="h-10 w-20 bg-white rounded-lg"></div>,
-    },
-    {
-      logo: <div className="h-10 w-20 bg-white rounded-lg"></div>,
-    },
-    {
-      logo: <div className="h-10 w-20 bg-white rounded-lg"></div>,
-    },
-    {
-      logo: <div className="h-10 w-20 bg-white rounded-lg"></div>,
-    },
-    {
-      logo: <div className="h-10 w-20 bg-white rounded-lg"></div>,
-    },
-    {
-      logo: <div className="h-10 w-20 bg-white rounded-lg"></div>,
-    },
-    {
-      logo: <div className="h-10 w-20 bg-white rounded-lg"></div>,
-    },
-    {
-      logo: <div className="h-10 w-20 bg-white rounded-lg"></div>,
-    },
-    {
-      logo: <div className="h-10 w-20 bg-white rounded-lg"></div>,
-    },
-    {
-      logo: <div className="h-10 w-20 bg-white rounded-lg"></div>,
-    },
-  ];
-  return (
-    <div className="mt-[8rem]  mx-auto">
-      <p className="text-[#48AECD] text-center text-base mb-10">
-        Trusted by the world's best Brands
-      </p>
-      <Marquee pauseOnHover className="flex gap-5">
-        <div className="flex items-center gap-5">
-          <div className="">
-            <div className="h-10 w-20 bg-white rounded-lg"></div>
-          </div>
-          {logos.map((logo) => {
-            return logo.logo;
-          })}
-        </div>
-      </Marquee>
-      <Marquee
-        direction="right"
-        pauseOnHover
-        className="mt-10 flex gap-5 items-center"
-      >
-        <div className="flex items-center gap-5 ">
-          {logos.map((logo) => {
-            return logo.logo;
-          })}
-        </div>
-      </Marquee>
-    </div>
-  );
+	const logos = [
+		{
+			logo: <div className="h-10 w-20 bg-white rounded-lg"></div>,
+		},
+		{
+			logo: <div className="h-10 w-20 bg-white rounded-lg"></div>,
+		},
+		{
+			logo: <div className="h-10 w-20 bg-white rounded-lg"></div>,
+		},
+		{
+			logo: <div className="h-10 w-20 bg-white rounded-lg"></div>,
+		},
+		{
+			logo: <div className="h-10 w-20 bg-white rounded-lg"></div>,
+		},
+		{
+			logo: <div className="h-10 w-20 bg-white rounded-lg"></div>,
+		},
+		{
+			logo: <div className="h-10 w-20 bg-white rounded-lg"></div>,
+		},
+		{
+			logo: <div className="h-10 w-20 bg-white rounded-lg"></div>,
+		},
+		{
+			logo: <div className="h-10 w-20 bg-white rounded-lg"></div>,
+		},
+		{
+			logo: <div className="h-10 w-20 bg-white rounded-lg"></div>,
+		},
+		{
+			logo: <div className="h-10 w-20 bg-white rounded-lg"></div>,
+		},
+		{
+			logo: <div className="h-10 w-20 bg-white rounded-lg"></div>,
+		},
+		{
+			logo: <div className="h-10 w-20 bg-white rounded-lg"></div>,
+		},
+		{
+			logo: <div className="h-10 w-20 bg-white rounded-lg"></div>,
+		},
+		{
+			logo: <div className="h-10 w-20 bg-white rounded-lg"></div>,
+		},
+	];
+	return (
+		<div className="mt-[8rem]  mx-auto">
+			<p className="text-[#48AECD] text-center font-semibold text-sm uppercase mb-10">
+				Trusted by the world's best Brands
+			</p>
+			<Marquee pauseOnHover className="flex gap-5">
+				<div className="flex items-center gap-5">
+					<div className="">
+						<div className="h-10 w-20 bg-white rounded-lg"></div>
+					</div>
+					{logos.map((logo) => {
+						return logo.logo;
+					})}
+				</div>
+			</Marquee>
+			<Marquee
+				direction="right"
+				pauseOnHover
+				className="mt-10 flex gap-5 items-center"
+			>
+				<div className="flex items-center gap-5 ">
+					{logos.map((logo) => {
+						return logo.logo;
+					})}
+				</div>
+			</Marquee>
+		</div>
+	);
 };
 
 export default Trust;
