45841c80edfd3e2c24525861ab25c92fd7d74068
eslamheshamm <esl2mhesham@gmail.com>
2023-08-31 15:01:24 +0300

    adjusting logos

--- src/componentws/trust.jsx
+++ src/componentws/trust.jsx
@@ -2,50 +2,49 @@ import Marquee from "react-fast-marquee";
 
 const Trust = () => {
 	const logosRowOne = [];
-	for (let i = 1; i <= 18; i++) {
+	const logosRowTwo = [];
+	for (let i = 1; i <= 16; i++) {
 		logosRowOne.push({
 			logo: (
-				<div className="h-14 w-20 rounded-lg bg-white">
+				<figure className="h-12 w-20 bg-white flex items-center justify-center rounded-lg">
 					<img src={`/logos/${i}.svg`} className="h-full w-full rounded-lg" />
-				</div>
+				</figure>
 			),
 		});
 	}
-	const logosRowTwo = [];
-	for (let i = 1; i <= 19; i++) {
+	for (let i = 1; i <= 16; i++) {
 		logosRowTwo.push({
 			logo: (
-				<div className="h-14 w-20 rounded-lg bg-white flex items-center justify-center">
-					<img src={`/logos/rowTwo/${i}.png`} className=" rounded-lg" />
-				</div>
+				<figure className="h-12 w-20 bg-white flex items-center justify-center rounded-lg p-2">
+					<img src={`/logos/rowTwo/${i}.png`} className=" rounded-lg p-2" />
+				</figure>
 			),
 		});
 	}
 	return (
 		<div className="mt-20">
-			<div className=" max-w-fit mx-auto ">
-				<p className="text-[#48AECD] text-center font-semibold text-sm uppercase mb-10">
-					Trusted by the world&apos;s best Brands
-				</p>
-			</div>
-			<Marquee pauseOnHover className="flex gap-5">
+			<p className="text-[#48AECD] text-center font-semibold text-sm uppercase mb-10">
+				Trusted by the world&apos;s best Brands
+			</p>
+			<Marquee
+				direction="left"
+				pauseOnHover
+				className="flex gap-5 items-center"
+			>
 				<div className="flex items-center gap-5">
-					<div className="">
-						<div className="h-10 w-20 bg-white rounded-lg"></div>
-					</div>
-					{logosRowOne.map((logo) => {
-						return logo.logo;
+					{logosRowOne.map(({ logo }) => {
+						return logo;
 					})}
 				</div>
 			</Marquee>
 			<Marquee
 				direction="right"
 				pauseOnHover
-				className="mt-10 flex gap-5 items-center"
+				className="flex gap-5 items-center mt-10"
 			>
 				<div className="flex items-center gap-5 ">
-					{logosRowTwo.map((logo) => {
-						return logo.logo;
+					{logosRowTwo.map(({ logo }) => {
+						return logo;
 					})}
 				</div>
 			</Marquee>
