11f8d2b57b316be44871438cc527e757542be4d0
Neptune669 <89285566+Neptune669@users.noreply.github.com>
2023-09-26 18:03:56 +0300

    push

--- src/componentws/form.jsx
+++ src/componentws/form.jsx
@@ -77,11 +77,14 @@ export default function App() {
         placeholder="message"
         className="inline-flex items-start justify-start w-full h-48 p-4 mt-10 bg-black border rounded-lg placeholder:text-white placeholder:font-bold placeholder:text-lg border-zinc-800"
       ></textarea>
-      <div className="inline-flex items-start justify-center h-16 px-8 py-6 bg-white border border-white shadow w-60 rounded-3xl">
-        <div className="text-lg font-semibold leading-tight tracking-wide text-center uppercase text-zinc-950">
+      <label className="inline-flex items-start justify-center h-16 px-8 py-6 bg-white border border-white shadow w-60 rounded-3xl">
+        <button
+          type="submit"
+          className="text-lg font-semibold leading-tight tracking-wide text-center uppercase text-zinc-950"
+        >
           Send Message
-        </div>
-      </div>
+        </button>
+      </label>
       {/* include validation with required or other standard HTML validation rules */}
       {/* <input {...register("exampleRequired", { required: true })} /> */}
       {/* errors will return when field validation fails  */}
