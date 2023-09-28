8aa46f8f2e3cd68061b245a10fc361fc98e3392e
Neptune669 <89285566+Neptune669@users.noreply.github.com>
2023-09-28 20:40:01 +0300

    contact form responsive fix

--- src/componentws/form.jsx
+++ src/componentws/form.jsx
@@ -80,7 +80,7 @@ export default function App() {
         placeholder="message"
         className="inline-flex items-start justify-start w-full h-48 p-4 mt-10 bg-black border rounded-lg placeholder:text-white placeholder:font-bold placeholder:text-lg border-zinc-800"
       ></textarea>
-      <label className="inline-flex items-start justify-center h-16 px-8 py-6 bg-white border border-white shadow w-60 rounded-3xl">
+      <label className="inline-flex items-center justify-center h-12 px-4 py-4 bg-white border border-white shadow lg:h-16 lg:px-8 lg:py-6 w-52 lg:w-60 rounded-3xl">
         <button
           type="submit"
           className="text-lg font-semibold leading-tight tracking-wide text-center uppercase text-zinc-950"
